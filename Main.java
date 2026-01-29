bbb43dcd64d58241bdb77fe272ae134a2e84a68b
Suryansh Shah <suryanshvishnoi0767@gmail.com>
2026-01-29 18:19:20 +0530

    29th January

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+    //static int n;
+    //int n;
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+        Main m = new Main();
+        int n;
+        System.out.println("Enter n");
+        n = scr.nextInt();
+        //m.n = scr.nextInt();
+        //System.out.println(m.n);
+        System.out.println(n);
+        scr.close();
+    }
+}
---
+++ Test.java
@@ -0,0 +1,16 @@
+class Test{
+    int n;
+    int Initialise(int m){
+    n=m;
+    return n;
+    }
+}
+
+// class Assign extends Test {
+//     public static void main(String [] args)
+//     {
+//         Assign ob = new Assign();
+//         int n = super.Initialise(5);
+//         System.out.println(n);
+//     }    
+// }
